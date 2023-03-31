556f5f0267533260332e884f8ba33d7f5d55d2e2
Paras Jain <64364245+ParasJain013@users.noreply.github.com>
2023-04-01 00:50:02 +0530

    Setup Complete

---
+++ backend/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const uri = "mongodb://localhost:27017";
+const connectMongo=()=>{
+    mongoose.connect(uri).then(()=>{
+        console.log("Connected SuccessFully  .......   MONGO")
+    })
+}
+module.exports = connectMongo;
